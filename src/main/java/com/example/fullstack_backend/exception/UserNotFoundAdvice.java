3098dd12454fa5bab905a37cb961e1bd5bf0547d
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 20:25:44 +0530

    create ExceptionHandler

--- src/main/java/com/example/fullstack_backend/exception/UserNotFoundAdvice.java
+++ src/main/java/com/example/fullstack_backend/exception/UserNotFoundAdvice.java
@@ -1,4 +1,17 @@
 package com.example.fullstack_backend.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class UserNotFoundAdvice {
+
+    public Map<String, String> exceptionHandler(UserNotFoundException exception){
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+
+        return errorMap;
+    }
 }

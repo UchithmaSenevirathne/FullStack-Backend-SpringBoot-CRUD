8d909072e5352370c08dcc76ae63e9ef7444ed5f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 16:14:36 +0530

    create user model

---
+++ src/main/java/com/example/fullstack_backend/model/User.java
@@ -0,0 +1,47 @@
+package com.example.fullstack_backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String name;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

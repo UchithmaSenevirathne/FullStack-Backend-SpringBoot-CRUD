b17971a99d5f7ff3f2a974f1e625b1663e3bee90
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 20:42:01 +0530

    delete user

--- src/main/java/com/example/fullstack_backend/controller/UserController.java
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -41,4 +41,13 @@ public class UserController {
                     return userRepository.save(user);
                 }).orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if (!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "user with id " + id + " has been deleted successfully";
+    }
 }
